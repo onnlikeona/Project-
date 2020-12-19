16666697625c35f3b778532ec10bb71c9fc25aa4
onnlikeona <71729620+onnlikeona@users.noreply.github.com>
2020-12-19 23:36:39 +0200

    Delete Stars_Citation.R

--- Stars_Citation.R
+++
@@ -1,6 +0,0 @@
-stars_citation <- function()
-{
-  print("Stirnimann, L., Conversi, A., and Marini, S. 2019. Detection of regime shifts in the environment: testing “STARS” using synthetic and observed time series. ICES Journal of Marine Science.")
-  print("Rodionov, S. and Overland, J.E., 2005. Application of a sequential regime shift detection method to the Bering Sea ecosystem. ICES Journal of Marine Science, 62(3), pp.328-332.")
-  print("Rodionov, S.N., 2004. A sequential algorithm for testing climate regime shifts. Geophysical Research Letters, 31(9).")
-}
